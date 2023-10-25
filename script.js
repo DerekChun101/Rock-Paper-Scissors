7d0297e66ca9ff7c8faa52284e2b207c32c1bf90
Derek Chun <derekchun2013@gmail.com>
2023-10-25 19:52:04 -0400

    Created player choice alogorithim

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    if(choice === 0)
+    if(choice === 0) {
         console.log("Rock");
-    else if(choice === 1)
+        return "rock";    
+    }
+    else if(choice === 1) {
         console.log("Paper");
-    else
+        return "paper";
+    }
+    else {
         console.log("Scissors");
+        return "scissors";
+    }
 }
 
-getComputerChoice();
+function getPlayerChoice() {
+    const playerChoice = prompt("Choose between rock, paper, or scissors").toLowerCase();
+    switch(playerChoice) {
+        case "rock":
+            return "rock";
+            break;
+        case "paper":
+            return "paper";
+            break;
+        case "scissors":
+            return "scissors";
 
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+playRound(playerSelection, computerSelection);
